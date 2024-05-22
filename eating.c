5ac7fd063a5aec237f0bc56cd38fd3406d2ea6c9
dinosnake666 <158581931+dinosnake666@users.noreply.github.com>
2024-05-22 12:29:11 +0100

    working on actions

--- eating.c
+++ eating.c
@@ -1,21 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 int main()
 {
     int hp = 50;
-    int breadAmount;
     int day;
-    char eatBread;
+    int sanity = 50;
+    char eating;
+    loop:
+    printf("You have found [bread]. Would you like to eat ? [Y/N] :");
+    scanf("%c", &eating);
 
-    printf("Current HP : %d\n", hp);
-    printf("Do you want to eat bread ? [Y/N] :");
-    scanf("%c", eatBread);
 
     for (day= 5 ; day > 0; day--)
     {
-        hp = hp - 10;
-        printf("Current HP : %d\n", hp);
+        
+        if (eating == 'Y' && hp < 50)
+        {
+            hp = hp + 10;
+        }
+        else if (eating == 'N')
+        {
+            hp = hp - 10;
+            printf("Current HP : %d\n", hp);
+        }
+        
+        
+        /*sanity = sanity- 5;
+        printf("Current sanity: %d\n", sanity);*/
+        sleep(3);
+        goto loop;
+
     }
     
     return 0;
